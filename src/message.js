f09d98824e0574a43bd251892d4112a9a983c87d
David Rosson <david@rosson.com.au>
2015-09-08 14:36:44 -0700

    Add Message prototype with methods

--- src/message.js
+++ src/message.js
@@ -1,9 +1,46 @@
+var Attachment = require('./attachment');
+var _ = require('underscore');
+
+_.templateSettings = { interpolate: /\{\{(.+?)\}\}/g };
+
 var Message = function(json) {
-  this.json = json;
+  this.json = null;
+  this.render = null;
+  this.attachments = [];
+  this.defaults = {
+    persona: {
+      name: 'Ferd',
+      icon: 'http://i.imgur.com/2rSS5KP.jpg'
+    }
+  };
 };
 
-Message.prototype.text = function() {
-  return this.json.text;
+Message.prototype.template = function(schema) {
+  this.render = _.template(schema);
+  return this;
+};
+
+Message.prototype.text = function(params) {
+  this.json.text = this.render(params);
+  return this;
+};
+
+Message.prototype.as = function(persona) {
+  this.json.username = persona.name || this.defaults.persona.name;
+  this.json.icon_url = persona.icon || this.defaults.persona.icon;
+  return this;
+};
+
+Message.prototype.attachments = function() {
+  return this.attachments;
+};
+
+Message.prototype.attach = function(attachment) {
+  // appends an attachment to the message
+};
+
+Message.prototype.detach = function() {
+  // removes all attachments
 };
 
 Message.prototype.user = function() {
